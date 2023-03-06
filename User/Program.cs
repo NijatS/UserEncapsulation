44a547d8d67b79a29c6d93d3fea60eaa150ad933
NijatS <nijatjs@code.edu.az>
2023-03-06 19:29:57 +0400

    done

---
+++ User/Program.cs
@@ -0,0 +1,19 @@
+﻿
+while (true)
+{
+    Console.Write("Please enter Username : ");
+    string username = Console.ReadLine();
+    Console.Write("Password : ");
+    string password = Console.ReadLine();
+    User user1 = new User(username, password);
+    if (user1.Password != null && user1.UserName != null)
+    {
+        Console.WriteLine("Successfull Access!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Password must contain upper case,min 8 car. and digit.\nUsername and Password don`t contain spaces");
+    }
+}
+
---
+++ User/User.cs
@@ -0,0 +1,88 @@
+﻿
+internal class User
+{
+    private string _username;
+    private string _password;
+    public string UserName
+    {
+        get
+        {
+            return _username;
+        }
+        set
+        {
+            if (!value.Contains(" "))
+            {
+                _username = value;
+            }
+        }
+
+    }
+    public string Password
+    {
+        get => _password; set
+        {
+            if (CheckPassword(value))
+            {
+                _password = value;
+            }
+            else
+            {
+                Console.WriteLine("Password must contain digit and upper case");
+
+            }
+        }
+
+    }
+    public User(string userName, string password)
+    {
+        if (CheckPassword(password))
+        {
+            UserName = userName;
+            Password = password;
+        }
+
+    }
+    public bool CheckPassword(string password)
+    {
+        if (password.Contains(" "))
+        {
+            return false;
+        }
+        if (password.Length >= 8)
+        {
+            int count1 = 0;
+            int count2 = 0;
+            for (int i = 0; i <= 9; i++)
+            {
+                string letter = i.ToString();
+
+                if (password.Contains(letter))
+                {
+                    count1++;
+                }
+            }
+            if (count1 == 0)
+            {
+                return false;
+            }
+
+            for (int i = 65; i <= 90; i++)
+            {
+                char c = (char)i;
+                string letter = c.ToString();
+                if (password.Contains(letter))
+                {
+                    count2++;
+                }
+            }
+            if (count2 == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        return false;
+
+    }
+}
